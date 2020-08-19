009ccfd36205b5aa67832f2a70ec34a5603a2b77
Taylor Robins <robinsnom@gmail.com>
2020-08-18 19:56:59 -0600

    Search Bar, and loading spinner

--- src/App.js
+++ src/App.js
@@ -9,13 +9,9 @@ import {
 } from "./ProductsService";
 import Modal from "./components/Modal";
 import AddEditProductForm from "./components/AddEditProductForm";
+import LoadingSpinner from "./components/LoadingSpinner";
 
 function App() {
-  const [products, setProducts] = React.useState(() => {
-    fetchProducts();
-
-    return [];
-  });
   const [
     isShowingAddEditProductModal,
     setIsShowingAddEditProductModal,
@@ -23,13 +19,48 @@ function App() {
 
   const [currentProduct, setCurrentProduct] = React.useState(null);
 
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [originalProducts, setOriginalProducts] = React.useState([]);
+  const [products, setProducts] = React.useState(() => {
+    fetchProducts();
+
+    return [];
+  });
+
+  const [searchQuery, setSearchQuery] = React.useState("");
+  React.useEffect(() => {
+    if (!searchQuery) {
+      setProducts(originalProducts);
+      return;
+    }
+
+    const filteredProducts = originalProducts.filter((product) => {
+      const searchQueryLowerCase = searchQuery.toLowerCase();
+      const productNameLowerCase = product.name.toLowerCase();
+
+      if (
+        productNameLowerCase.startsWith(searchQueryLowerCase) ||
+        productNameLowerCase.includes(searchQueryLowerCase)
+      ) {
+        return true;
+      }
+    });
+    setProducts(filteredProducts);
+  }, [searchQuery]);
+
   function fetchProducts() {
+    setIsLoading(true);
+
     getProducts()
       .then((response) => {
+        setOriginalProducts(response.data);
         setProducts(response.data);
       })
       .catch((error) => {
         debugger;
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   }
   function handleAddProductClick() {
@@ -97,7 +128,18 @@ function App() {
         </Modal>
       ) : null}
       <h1>React Grocery App</h1>
+      <input
+        type="text"
+        className="search-input"
+        value={searchQuery}
+        onChange={(e) => {
+          setSearchQuery(e.target.value);
+        }}
+      />
+      {isLoading ? <LoadingSpinner /> : null}
+
       <ProductGrid products={products} handleEditProduct={handleEditProduct} />
+      {!isLoading && products.length === 0 ? <h3>No Results Found</h3> : null}
     </div>
   );
 }
